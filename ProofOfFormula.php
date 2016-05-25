3645820ba93968097b7e73ad077a4902bc79ed8d
Christiaan Nieuwlaat <krizzje@Gmail.com>
2016-05-25 16:35:35 +0200

    Update ProofOfFormula.php
    Fixed wrong indexers

--- ProofOfFormula.php
+++ ProofOfFormula.php
@@ -9,8 +9,8 @@ waarbij f(1) = 1 en f(2) = 1 voor alle x > 0
 schrijf hiervoor een functie welke _het bewijs laat zien_ op het scherm dat f(10) = 55
 
 hint:
-  - schrijf de complete functieboom uit welke behoort bij f(x), bijv f(2)==> f(1) + f(0)
-  - als er alleen nog f(1) en f(0) termen zijn kun je deze vervangen door 1
+  - schrijf de complete functieboom uit welke behoort bij f(x), bijv f(3)==> f(2) + f(1)
+  - als er alleen nog f(2) en f(1) termen zijn kun je deze vervangen door 1
   - de weergave kan er als volgt uit zien:
   
   f(10) = f(9) + f(8)
